8b9bdb17ccf4412f186e29cb256a9db9aba6e449
PhKhrystyna <pihurchristina@gmail.com>
2021-01-13 14:24:21 -0500

    new pachage "Khrystyna" was creates & added new Class "HowYouDoing"

---
+++ src/test/java/com/nextBaseCRM/tests/Khrystyna/HowYouDoing.java
@@ -0,0 +1,35 @@
+package com.nextBaseCRM.tests.Khrystyna;
+
+public class HowYouDoing {
+    public static void main(String[] args) {
+
+        //Hey guys =))
+        //Congrats with OUR FIRST PROJECT =)))
+
+
+        /*
+                                            *      *
+                                           ***    ***
+                                          ****** ******
+                                         ***************
+                                        *****************
+                                        *****************
+                                         ***************
+                                          *************
+                                           ***********
+                                            *********
+                                             *******
+                                              *****
+                                               ***
+                                                *
+
+
+
+
+         */
+
+
+
+
+    }
+}
