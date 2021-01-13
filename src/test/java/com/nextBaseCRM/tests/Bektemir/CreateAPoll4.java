0e1ecef82f3d8e136d74b41821a8f425563aa0a0
Beka <bektemir.abdymomunov@mail.ru>
2021-01-13 17:13:32 -0500

    setup my driver

--- src/test/java/com/nextBaseCRM/tests/Bektemir/CreateAPoll4.java
+++ src/test/java/com/nextBaseCRM/tests/Bektemir/CreateAPoll4.java
@@ -1,5 +1,20 @@
 package com.nextBaseCRM.tests.Bektemir;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
 public class CreateAPoll4 {
+    @BeforeClass
+    public void setupDriver(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+
 
 }
