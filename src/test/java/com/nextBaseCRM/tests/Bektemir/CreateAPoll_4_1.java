83c082a6edac0aa6915fbb4f92a6cb483fb79cac
Beka <bektemir.abdymomunov@mail.ru>
2021-01-12 21:12:02 -0500

    tried to push some changes

--- src/test/java/com/nextBaseCRM/tests/Bektemir/CreateAPoll_4_1.java
+++ src/test/java/com/nextBaseCRM/tests/Bektemir/CreateAPoll_4_1.java
@@ -3,4 +3,5 @@ package com.nextBaseCRM.tests.Bektemir;
 public class CreateAPoll_4_1 {
     //Hey , guys ! Glad to see you here !
     //Please create new package with your name and you will be writing your automation codes in there
+    //I'm trying to push some changes
 }
