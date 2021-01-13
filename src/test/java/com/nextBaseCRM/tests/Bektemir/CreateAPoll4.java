02a2691c3bec0ebd2505682821f25fc232753369
Beka <bektemir.abdymomunov@mail.ru>
2021-01-13 17:17:20 -0500

    added closeDriver method

--- src/test/java/com/nextBaseCRM/tests/Bektemir/CreateAPoll4.java
+++ src/test/java/com/nextBaseCRM/tests/Bektemir/CreateAPoll4.java
@@ -1,19 +1,29 @@
 package com.nextBaseCRM.tests.Bektemir;
 
+import com.nextBaseCRM.tests.Bektemir.utilities.WebDriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import java.util.concurrent.TimeUnit;
 
 public class CreateAPoll4 {
+    WebDriver driver;
+
     @BeforeClass
     public void setupDriver(){
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+
+    @AfterClass
+    public void closeDriver(){
+    WebDriverFactory.sleep(5);
+    driver.close();
     }
 
 
