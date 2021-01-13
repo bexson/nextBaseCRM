cb64b962c6e380fa12a763796007f92ab9e1c86a
Beka <bektemir.abdymomunov@mail.ru>
2021-01-12 21:09:58 -0500

    added some advices

--- src/test/java/com/nextBaseCRM/tests/Bektemir/CreateAPoll_4_1.java
+++ src/test/java/com/nextBaseCRM/tests/Bektemir/CreateAPoll_4_1.java
@@ -2,4 +2,5 @@ package com.nextBaseCRM.tests.Bektemir;
 
 public class CreateAPoll_4_1 {
     //Hey , guys ! Glad to see you here !
+    //Please create new package with your name and you will be writing your automation codes in there
 }
