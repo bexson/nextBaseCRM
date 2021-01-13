ba9a66fd14637b621330f8c17324b485db66a81a
Beka <bektemir.abdymomunov@mail.ru>
2021-01-12 21:14:24 -0500

    added some message

--- src/test/java/com/nextBaseCRM/tests/Bektemir/CreateAPoll_4_1.java
+++ src/test/java/com/nextBaseCRM/tests/Bektemir/CreateAPoll_4_1.java
@@ -4,4 +4,5 @@ public class CreateAPoll_4_1 {
     //Hey , guys ! Glad to see you here !
     //Please create new package with your name and you will be writing your automation codes in there
     //I'm trying to push some changes
+    // Let's have fun with automation testing and Github !!! :)
 }
