06aa912c0ebcd091253876eb59b000220c8f286e
Beka <bektemir.abdymomunov@mail.ru>
2021-01-16 20:19:41 -0500

    made some changes

--- src/test/java/com/nextBaseCRM/tests/Bektemir/CreateAPoll4.java
+++ src/test/java/com/nextBaseCRM/tests/Bektemir/CreateAPoll4.java
@@ -16,34 +16,19 @@ import java.util.concurrent.TimeUnit;
 public class CreateAPoll4 {
     WebDriver driver;
 
-    public void manageWebDriver(WebDriver driver){
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-    }
-    public void findElementXpath(String stringXpath, WebDriver driver){
-        driver.findElement(By.xpath(stringXpath));
-    }
-    public void sleep(long seconds){
-        try {
-            Thread.sleep(seconds*1000);
-        }catch (InterruptedException e){
-            System.out.println("Can't handle with Interrupted exception");
-        }
-    }
-
     @BeforeClass
     public void setupDriver(){
        WebDriverManager.chromedriver().setup();
        driver = new ChromeDriver();
 
-       manageWebDriver(driver);
+       WebDriverFactory.manageWebDriver(driver);
        driver.get("https://login2.nextbasecrm.com/stream/");
-      sleep(2);
+      WebDriverFactory.sleep(2);
     }
 
     @AfterClass
     public void closeDriver(){
-        sleep(2);
+        WebDriverFactory.sleep(2);
         driver.close();
     }
 
@@ -67,44 +52,44 @@ public class CreateAPoll4 {
     public void test_execution_forUS4(){
 //==================================DOING STEP 1 IN MY TEST EXECUTION====================================================
         driver.findElement(By.id("feed-add-post-form-tab-vote")).click();
-        sleep(2);
+       WebDriverFactory.sleep(2);
 //==================================DOING STEP 2 IN MY TEST EXECUTION====================================================
-//      driver.switchTo().frame(0);
-//      WebElement mainField = driver.findElement(By.xpath("//body[@contenteditable='true']"));
-//      mainField.sendKeys("Testing My Poll");
-//      driver.switchTo().defaultContent();
-//      sleep(5);
+     driver.switchTo().frame(0);
+     WebElement mainField = driver.findElement(By.xpath("//body[@contenteditable='true']"));
+     mainField.sendKeys("Testing My Poll");
+     driver.switchTo().defaultContent();
+       WebDriverFactory.sleep(5);
 //==================================DOING STEP 3 IN MY TEST EXECUTION====================================================
-//     WebElement addMoreElement = driver.findElement(By.xpath("//a[.='Add more']"));
-//     addMoreElement.click();
-//     sleep(3);
-//     driver.findElement(By.xpath("//div[.='Sevara Isroilova']/div")).click();
-//     sleep(3);
-//     driver.findElement(By.xpath("//span[@class='popup-window-close-icon']")).click();
-//        sleep(3);
+   WebElement addMoreElement = driver.findElement(By.xpath("//a[.='Add more']"));
+   addMoreElement.click();
+       WebDriverFactory.sleep(3);
+   driver.findElement(By.xpath("//div[.='Sevara Isroilova']/div")).click();
+       WebDriverFactory.sleep(3);
+   driver.findElement(By.xpath("//span[@class='popup-window-close-icon']")).click();
+       WebDriverFactory.sleep(3);
 //==================================DOING STEP 4 IN MY TEST EXECUTION====================================================
-//       WebElement questionFIeld = driver.findElement(By.cssSelector("input#question_0"));
-//       questionFIeld.sendKeys("Who is the best soccer player in 2020?");
-//       sleep(3);
+       WebElement questionFIeld = driver.findElement(By.cssSelector("input#question_0"));
+       questionFIeld.sendKeys("Who is the best soccer player in 2020?");
+       WebDriverFactory.sleep(3);
 //==//===============================DOING STEP 5 IN MY TEST EXECUTION====================================================
-//      WebElement answerField = driver.findElement(By.id("answer_0__0_"));
-//      answerField.sendKeys("Cristiano Ronaldo");
-//       sleep(4);
+     WebElement answerField = driver.findElement(By.id("answer_0__0_"));
+     answerField.sendKeys("Cristiano Ronaldo");
+       WebDriverFactory.sleep(4);
 //==//===============================DOING STEP 6 IN MY TEST EXECUTION====================================================
-//         WebElement checkbox = driver.findElement(By.id("multi_0"));
-//         checkbox.click();
-//         sleep(3);
+         WebElement checkbox = driver.findElement(By.id("multi_0"));
+         checkbox.click();
+       WebDriverFactory.sleep(3);
 //==//===============================DOING STEP 7 IN MY TEST EXECUTION====================================================
-//       WebElement addQuestion = driver.findElement(By.xpath("//a[.='Add question']"));
-//       addQuestion.click();
-//        sleep(4);
+      WebElement addQuestion = driver.findElement(By.xpath("//a[.='Add question']"));
+      addQuestion.click();
+       WebDriverFactory.sleep(4);
 //==//===============================DOING STEP 8 IN MY TEST EXECUTION====================================================
-//       WebElement sendButton = driver.findElement(By.cssSelector("button#blog-submit-button-save"));
-//       sendButton.click();
-//      sleep(5);
+      WebElement sendButton = driver.findElement(By.cssSelector("button#blog-submit-button-save"));
+      sendButton.click();
+       WebDriverFactory.sleep(5);
 //==================================DOING STEP 9 IN MY TEST EXECUTION====================================================
-     WebElement cancelButton = driver.findElement(By.xpath("//*[@class='ui-btn ui-btn-lg ui-btn-link']"));
-     cancelButton.click();
+   // WebElement cancelButton = driver.findElement(By.xpath("//*[@class='ui-btn ui-btn-lg ui-btn-link']"));
+   // cancelButton.click();
     }
 
 
