a387c1c8042f4724418ed63dd6f4655a9920c91b
Mariana991 <ma7643@yahoo.com>
2021-01-21 18:24:37 -0500

    Hi group 17!

--- src/test/java/com/nextBaseCRM/tests/Mariana/Hi_Guys.java
+++ src/test/java/com/nextBaseCRM/tests/Mariana/Hi_Guys.java
@@ -3,5 +3,6 @@ package com.nextBaseCRM.tests.Mariana;
 public class Hi_Guys {
     public static void main(String[] args) {
         String a = "Hi everyone!";
+        String b = "Hi group 17";
     }
 }
