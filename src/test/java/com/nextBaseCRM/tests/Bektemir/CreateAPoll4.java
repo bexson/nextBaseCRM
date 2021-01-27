e5bd7ac2ff5e86f75e1b4a1fe1b43a0b9cbde971
Beka <bektemir.abdymomunov@mail.ru>
2021-01-27 01:04:58 -0500

    inherited my TestBase.java, made my codes more clear

--- src/test/java/com/nextBaseCRM/tests/Bektemir/CreateAPoll4.java
+++ src/test/java/com/nextBaseCRM/tests/Bektemir/CreateAPoll4.java
@@ -2,6 +2,7 @@ package com.nextBaseCRM.tests.Bektemir;
 
 import com.nextBaseCRM.tests.Bektemir.utilities.ConfigurationReader;
 import com.nextBaseCRM.tests.Bektemir.utilities.WebDriverFactory;
+import com.nextBaseCRM.tests.base.TestBase;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,86 +15,79 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class CreateAPoll4 {
-    WebDriver driver;
-    public String url = ConfigurationReader.getProperty("url");
+public class CreateAPoll4 extends TestBase {
+    public final String url = ConfigurationReader.getProperty("url");
+    private final String username1 = ConfigurationReader.getProperty("username1");
+    private final String password = ConfigurationReader.getProperty("password");
 
     @BeforeClass
-    public void setupDriver(){
-       WebDriverManager.chromedriver().setup();
-       driver = new ChromeDriver();
-
-       WebDriverFactory.manageWebDriver(driver);
-       driver.get(url);
-      WebDriverFactory.sleep(2);
-    }
-
-    @AfterClass
-    public void closeDriver(){
-        WebDriverFactory.sleep(2);
-        driver.close();
+    public void getUrl() {
+        driver.get(url);
     }
 
     @Test
-    public void pre_condition_for_US4(){
+    public void pre_condition_for_US4() {
 //==================================PRE-CONDITION FOR MY USER STORY4====================================================
-         String expectedTitle = "Authorization";
-         String actualTitle = driver.getTitle();
-         Assert.assertEquals(expectedTitle,actualTitle);
-         String username1= ConfigurationReader.getProperty("username1");
-         String password = ConfigurationReader.getProperty("password");
+        String expectedTitle = "Authorization";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(expectedTitle, actualTitle);
 
-         driver.findElement(By.name("USER_LOGIN")).sendKeys(username1); //username
+        driver.findElement(By.name("USER_LOGIN")).sendKeys(username1); //username
         driver.findElement(By.name("USER_PASSWORD")).sendKeys(password); //password
         driver.findElement(By.className("login-btn")).click();//click login button
-        String expectedURL = url;
         String actualURL = driver.getCurrentUrl();
-        Assert.assertEquals(expectedURL,actualURL);
+        Assert.assertEquals(url, actualURL);
 //==================================FINISHED WITH MY PRE-CONDITION======================================================
     }
 
-   @Test
-    public void test_execution_forUS4(){
+    @Test
+    public void test_execution_forUS4() {
 //==================================DOING STEP 1 IN MY TEST EXECUTION====================================================
         driver.findElement(By.id("feed-add-post-form-tab-vote")).click();
-       WebDriverFactory.sleep(2);
+        WebDriverFactory.sleep(2);
 //==================================DOING STEP 2 IN MY TEST EXECUTION====================================================
-     driver.switchTo().frame(0);
-     WebElement mainField = driver.findElement(By.xpath("//body[@contenteditable='true']"));
-     mainField.sendKeys("Testing My Poll");
-     driver.switchTo().defaultContent();
-       WebDriverFactory.sleep(5);
+        driver.switchTo().frame(0);
+        WebElement mainField = driver.findElement(By.xpath("//body[@contenteditable='true']"));
+        mainField.sendKeys("Testing My Poll");
+        boolean isDisplayed = driver.findElement(By.xpath("//body[.='Testing My Poll']")).isDisplayed();
+        Assert.assertTrue(isDisplayed);
+        driver.switchTo().defaultContent();
+        WebDriverFactory.sleep(5);
 //==================================DOING STEP 3 IN MY TEST EXECUTION====================================================
-   WebElement addMoreElement = driver.findElement(By.xpath("//a[.='Add more']"));
-   addMoreElement.click();
-       WebDriverFactory.sleep(3);
-   driver.findElement(By.xpath("//div[.='helpdesk26@cybertekschool.com']//div")).click();
-       WebDriverFactory.sleep(3);
-   driver.findElement(By.xpath("//span[@class='popup-window-close-icon']")).click();
-       WebDriverFactory.sleep(3);
+        WebElement addMoreElement = driver.findElement(By.xpath("//a[.='Add more']"));
+        addMoreElement.click();
+        WebDriverFactory.sleep(3);
+        driver.findElement(By.xpath("//div[.='helpdesk26@cybertekschool.com']//div")).click();
+        boolean isDisplayedHD26 = driver.findElement(By.xpath("//span[.='helpdesk26@cybertekschool.com']//span[1]")).isDisplayed();
+        Assert.assertTrue(isDisplayedHD26);
+        WebDriverFactory.sleep(3);
+        driver.findElement(By.xpath("//span[@class='popup-window-close-icon']")).click();
+        WebDriverFactory.sleep(3);
 //==================================DOING STEP 4 IN MY TEST EXECUTION====================================================
-       WebElement questionFIeld = driver.findElement(By.cssSelector("input#question_0"));
-       questionFIeld.sendKeys("Who is the best soccer player in 2020?");
-       WebDriverFactory.sleep(3);
-//==//===============================DOING STEP 5 IN MY TEST EXECUTION====================================================
-     WebElement answerField = driver.findElement(By.id("answer_0__0_"));
-     answerField.sendKeys("Cristiano Ronaldo");
-       WebDriverFactory.sleep(4);
-//==//===============================DOING STEP 6 IN MY TEST EXECUTION====================================================
-         WebElement checkbox = driver.findElement(By.id("multi_0"));
-         checkbox.click();
-       WebDriverFactory.sleep(3);
-//==//===============================DOING STEP 7 IN MY TEST EXECUTION====================================================
-      WebElement addQuestion = driver.findElement(By.xpath("//a[.='Add question']"));
-      addQuestion.click();
-       WebDriverFactory.sleep(4);
-//==//===============================DOING STEP 8 IN MY TEST EXECUTION====================================================
-      WebElement sendButton = driver.findElement(By.cssSelector("button#blog-submit-button-save"));
-      sendButton.click();
-       WebDriverFactory.sleep(5);
+        WebElement questionFIeld = driver.findElement(By.cssSelector("input#question_0"));
+        questionFIeld.sendKeys("Who is the best soccer player in 2020?");
+        WebDriverFactory.sleep(3);
+//=================================DOING STEP 5 IN MY TEST EXECUTION====================================================
+        WebElement answerField = driver.findElement(By.id("answer_0__0_"));
+        answerField.sendKeys("Cristiano Ronaldo");
+        WebDriverFactory.sleep(4);
+//=================================DOING STEP 6 IN MY TEST EXECUTION====================================================
+        WebElement checkbox = driver.findElement(By.id("multi_0"));
+        checkbox.click();
+        boolean checkboxIsSelected = checkbox.isSelected();
+        Assert.assertTrue(checkboxIsSelected);
+        WebDriverFactory.sleep(3);
+//=================================DOING STEP 7 IN MY TEST EXECUTION====================================================
+        WebElement addQuestion = driver.findElement(By.xpath("//a[.='Add question']"));
+        addQuestion.click();
+        WebDriverFactory.sleep(4);
+//=================================DOING STEP 8 IN MY TEST EXECUTION====================================================
+        WebElement sendButton = driver.findElement(By.cssSelector("button#blog-submit-button-save"));
+        sendButton.click();
+        WebDriverFactory.sleep(5);
 //==================================DOING STEP 9 IN MY TEST EXECUTION====================================================
-   // WebElement cancelButton = driver.findElement(By.xpath("//*[@class='ui-btn ui-btn-lg ui-btn-link']"));
-   // cancelButton.click();
+        // WebElement cancelButton = driver.findElement(By.xpath("//*[@class='ui-btn ui-btn-lg ui-btn-link']"));
+        // cancelButton.click();
     }
 
 }
--- src/test/java/com/nextBaseCRM/tests/base/TestBase.java
+++ src/test/java/com/nextBaseCRM/tests/base/TestBase.java
@@ -2,24 +2,24 @@ package com.nextBaseCRM.tests.base;
 
 import com.nextBaseCRM.tests.Bektemir.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 import java.util.concurrent.TimeUnit;
 
 public abstract class TestBase {
     public WebDriver driver;
+    // please guys don't change here anything
 
-
-    @BeforeMethod
-    public void setupMethod(){
+    @BeforeClass // this will run 1 time only before class
+    public void setupClass(){
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
     }
 
-    @AfterMethod
-    public void close(){
-
+    @AfterClass // this will run 1 time only after class
+    public void closeClass(){
+      driver.close();
     }
 }
