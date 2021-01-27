55deeb74d47eabea9497660fb82f75308725111a
BayanaSab <bayana90@yahoo.com>
2021-01-26 20:40:01 -0500

    Initial work

---
+++ src/test/java/com/nextBaseCRM/tests/Bayana/SendAnEvent.java
@@ -0,0 +1,15 @@
+package com.nextBaseCRM.tests.Bayana;
+
+import com.nextBaseCRM.tests.base.TestBase;
+import org.testng.annotations.Test;
+
+public class SendAnEvent extends TestBase {
+
+
+
+
+
+
+
+
+}
